c40742da9b99f181dfc377300eaac46ae8517d0f
cswitzer <50593813+cswitzer@users.noreply.github.com>
2021-03-25 21:42:27 -0600

    Enemy hit is now processed

---
+++ Tower Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int maxHitPoints = 5;
+    [SerializeField] int currentHitPoints = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    // Update is called once per frame
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        currentHitPoints -= 1;
+
+        if (currentHitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
